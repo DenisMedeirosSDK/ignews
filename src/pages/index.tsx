f9b38ff330dfa0e4fc60a294297fd2fe8b372b99
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-01-07 07:47:41 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>ig.news</title>
-      </Head>{' '}
-      <h1 className={styles.title}>Hello world</h1>
+      </Head>
+      <h1>Hello world</h1>
     </>
   );
 }
