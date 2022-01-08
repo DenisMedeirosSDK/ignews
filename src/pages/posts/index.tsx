20e89dcf34a41720ec33a47823c43842094c4151
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-01-08 10:13:11 -0300

    feat: create page post details

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,10 @@
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { GetStaticProps } from 'next';
+
 import * as prismic from '@prismicio/client';
-import { PrismicDocument } from '@prismicio/types';
 import { RichText } from 'prismic-dom';
+
 import { getPrimiscClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
@@ -25,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -48,7 +52,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const posts = documents.results.map(post => {
     return {
-      slug: post.id,
+      slug: post.uid,
       title: RichText.asText(post.data.title),
       excerpt:
         post.data.content.find(content => content.type === 'paragraph')?.text ??
